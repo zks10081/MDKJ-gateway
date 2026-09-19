e95c0989dd0ce92088f02185cedfd48cc3ce4412
zks10081 <727134914@qq.com>
2026-09-19 23:27:33 +0800

    进度到导航区

---
+++ getway/Base/Command.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace getway.Base
+{
+    class Command : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+
+            _action?.Invoke(parameter);
+        }
+
+        Action<Object> _action;
+        public Command(Action<Object> action)
+        {
+            _action = action;
+        }
+    }
+}
---
+++ getway/ViewModel/MainVlewModel.cs
@@ -0,0 +1,32 @@
+﻿using getway.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace getway.ViewModel
+{
+    class MainVlewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public object pageContent {  get; set; }
+
+        //命令属性
+        public ICommand NavCommand { get; set; }
+
+        public MainVlewModel() {
+            NavCommand = new Command(doNavPage);
+        }
+
+
+        private void doNavPage(object paramter)
+        {
+            pageContent = paramter;
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs("pageContent"));
+        }
+    }
+}
