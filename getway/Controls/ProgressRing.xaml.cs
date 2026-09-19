cb435add96cab7abc7da356361bc48d405e2fad8
zks10081 <727134914@qq.com>
2026-09-20 00:30:22 +0800

    删除加载文件，环装按钮

---
+++ getway/Controls/ProgressRing.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace getway.Controls
+{
+    /// <summary>
+    /// ProgressRing.xaml 的交互逻辑
+    /// </summary>
+    public partial class ProgressRing : UserControl
+    {
+        public List<MarkInfo> MarkList { get; set; } = new List<MarkInfo>();
+        public ProgressRing()
+        {
+            InitializeComponent();
+            int count = 360 / 8;
+            for (int i = 0; i < count; i++)
+            {
+                MarkList.Add(new MarkInfo() { Angle = i * 8 });
+            }
+        }
+    }
+
+    public class MarkInfo
+    {
+        public double Angle {  get; set; }
+        public Brush color {  get; set; }
+    }
+}
